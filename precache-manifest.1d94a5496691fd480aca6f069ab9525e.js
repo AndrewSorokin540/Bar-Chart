64834ec2224790a9059c3d08980940780caed3d8
Andrey Sorokin <andrewsorokin540@gmail.com>
2020-01-09 20:01:07 +0700

    Updates

---
+++ precache-manifest.1d94a5496691fd480aca6f069ab9525e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bece07d9a8c1c6b289705106db5aaf2d",
+    "url": "/bar-chart/index.html"
+  },
+  {
+    "revision": "b25ae9fc9333497bdf22",
+    "url": "/bar-chart/static/css/main.1761929e.chunk.css"
+  },
+  {
+    "revision": "421b37a9289c31da2d69",
+    "url": "/bar-chart/static/js/2.8057d0c0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/bar-chart/static/js/2.8057d0c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b25ae9fc9333497bdf22",
+    "url": "/bar-chart/static/js/main.e3d50cc5.chunk.js"
+  },
+  {
+    "revision": "2c40c79b9c4226a82ec9",
+    "url": "/bar-chart/static/js/runtime-main.5dbde2be.js"
+  }
+]);
